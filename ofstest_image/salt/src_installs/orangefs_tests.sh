005231842127ea77865697d4c54dd0c6bd5464f7
James Burton <jburto2@clemson.edu>
2015-10-23 16:53:41 -0400

    Make sure mpicc is in path before compiling orangefs tests.

--- ofstest_image/salt/src_installs/orangefs_tests.sh
+++ ofstest_image/salt/src_installs/orangefs_tests.sh
@@ -7,7 +7,7 @@ cd ~/orangefs_src/test
 # kludge because perfbase won't compile.
 rm -rf perfbase
 
-
+export PATH=$PATH:/opt/mpi/openmpi/bin
 CFLAGS='-g -O0' ./configure  --prefix=/opt/orangefs --with-db=/opt/db4 --with-mpi=/opt/mpi/openmpi --enable-strict --enable-shared && \
 make && \
 make install
