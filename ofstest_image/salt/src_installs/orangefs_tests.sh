87d8da91e64430c8ddf1bb5c656bcdf5614ffaf4
James Burton <jburto2@clemson.edu>
2015-10-26 14:54:48 -0400

    Remove perfbase from tests after configure, so configure will work.

--- ofstest_image/salt/src_installs/orangefs_tests.sh
+++ ofstest_image/salt/src_installs/orangefs_tests.sh
@@ -4,11 +4,12 @@
 set -x
 
 cd /opt/orangefs_src/test
-# kludge because perfbase won't compile.
-rm -rf perfbase
+
 
 export PATH=$PATH:/opt/mpi/openmpi/bin
 CFLAGS='-g -O0' ./configure  --prefix=/opt/orangefs --with-db=/opt/db4 --with-mpi=/opt/mpi/openmpi --enable-strict --enable-shared && \
+# kludge because perfbase won't compile.
+rm -rf perfbase
 make && \
 make install
 
