1c348555b97a11b93102f50c244e2df39e0a4805
James Burton <jburto2@clemson.edu>
2015-11-05 13:35:42 -0500

    Fix typo

--- ofstest_image/salt/src_installs/maven.sh
+++ ofstest_image/salt/src_installs/maven.sh
@@ -7,12 +7,8 @@
 set -x
 # Check to see if the package manager has already installed maven.
 ln -s /opt/maven /opt/apache-maven-3.2.5
-if which mvn
+if ! which mvn
 then	
    	ln -s /opt/apache-maven-3.2.5/bin/mvn /usr/bin/mvn
-   	
-    
-else
-	return 0
 fi
 
