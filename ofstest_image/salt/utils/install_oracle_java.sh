a71c42ae6be77408a8bdcb1ada1205ad7c14cdd1
JBURTO2 <jburto2@clemson.edu>
2016-06-01 16:49:18 -0400

    update IOR to use 4mb transfers. Also update install_oracle_java.sh to install Java 8 on ubuntu systems newer than trusty.

--- ofstest_image/salt/utils/install_oracle_java.sh
+++ ofstest_image/salt/utils/install_oracle_java.sh
@@ -5,5 +5,10 @@ add-apt-repository ppa:webupd8team/java < /dev/null
 apt-get update
 bash -c 'echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections'
 bash -c 'echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections'
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q oracle-java7-installer || apt-get install -y openjdk-7-jre openjdk-7-jre-lib
+if grep 'trusty' /etc/lsb-release
+then
+	DEBIAN_FRONTEND=noninteractive apt-get install -y -q oracle-java7-installer || apt-get install -y openjdk-7-jre openjdk-7-jre-lib
+else
+	DEBIAN_FRONTEND=noninteractive apt-get install -y -q oracle-java8-installer || apt-get install -y openjdk-8-jre openjdk-8-jre-lib
+fi
                 
