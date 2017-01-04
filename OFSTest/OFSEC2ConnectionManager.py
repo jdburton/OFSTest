5e472fdf30fad0fe0e00f9dc0a94b1dfa891afab
JBURTO2 <jburto2@clemson.edu>
2017-01-04 14:02:53 -0500

    Check to make sure we have all reservations before getting the instances.

--- OFSTest/OFSEC2ConnectionManager.py
+++ OFSTest/OFSEC2ConnectionManager.py
@@ -443,7 +443,14 @@ class OFSEC2ConnectionManager(OFSCloudConnectionManager.OFSCloudConnectionManage
                 return new_instances
             
             spot_instance_ids = [r.instance_id for r in fulfilled_requests]
-            reservations = self.ec2_connection.get_all_reservations(instance_ids=spot_instance_ids)
+            
+            reservations = []
+            count = 0
+            while len(reservations) < number_nodes  and count < 360:
+                reservations = self.ec2_connection.get_all_reservations(instance_ids=spot_instance_ids)
+                count += 1
+                time.sleep(5)
+                
             new_instances = [spot_res.instances[0] for spot_res in reservations]
             
             print spot_instance_ids
