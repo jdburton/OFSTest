1b0721b0d820b13d46f3a74feb2e3e866caf2e05
JBURTO2 <jburto2@clemson.edu>
2017-11-02 18:06:49 -0400

    Update orangefs_test.sh for openmpi updates

--- ofstest_image/salt/src_installs/orangefs_tests.sh
+++ ofstest_image/salt/src_installs/orangefs_tests.sh
@@ -6,8 +6,8 @@ set -x
 cd /opt/orangefs_src/test
 
 
-export PATH=$PATH:/opt/mpi/openmpi/bin
-CFLAGS='-g -O0' ./configure  --with-db-backend=bdb --prefix=/opt/orangefs --with-db=/opt/db4 --with-mpi=/opt/mpi/openmpi --enable-strict --enable-shared && \
+export PATH=$PATH:/opt/mpi/openmpi-1.8.8/bin
+CFLAGS='-g -O0' ./configure  --with-db-backend=bdb --prefix=/opt/orangefs --with-db=/opt/db4 --with-mpi=/opt/mpi/openmpi-1.8.8 --enable-strict --enable-shared && \
 # kludge because perfbase won't compile.
 rm -rf perfbase
 rm -rf io
